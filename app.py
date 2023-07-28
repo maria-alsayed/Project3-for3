6d3249b9ab1a88a845595c47bb6049518bb4d9f1
Kelly McDonalad <128551541+keljmcdonald@users.noreply.github.com>
2023-07-27 19:04:10 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,58 @@
-from flask import Flask, render_template, jsonify
-import sqlite3
+from flask import Flask, jsonify, render_template
+import pandas as pd
+from nba_api.stats.endpoints import teamyearbyyearstats
 
 app = Flask(__name__)
 
-# Function to fetch team statistics from the database
-def fetch_team_stats():
-    conn = sqlite3.connect('nba_stats.db')
-    cur = conn.cursor()
+# Define a dictionary to store team IDs and their corresponding names
+team_names = {
+    1610612739: 'Cleveland Cavaliers',
+    1610612738: 'Boston Celtics',
+    1610612759: 'Spurs',
+    1610612747: 'LA Lakers',
+    1610612741: 'Bulls',
+    1610612752: 'Knicks',
+    1610612744: 'Warriors',
 
-    teams_data = []
-    for team_id in [1610612759, 1610612752, 1610612744, 1610612737, 1610612738, 1610612747, 1610612739]:
-        cur.execute('''SELECT year, three_point_attempts, three_point_made, three_point_percentage, po_wins, wins, losses
-            FROM team_stats WHERE team_id=? ORDER BY year''', (team_id,))
-        team_data = cur.fetchall()
-        teams_data.append({
-            'team_id': team_id,
-            'team_data': team_data
+    # Add more teams here if needed
+}
+
+def get_team_data(team_id):
+    try:
+        # Retrieve team year-by-year statistics
+        team_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=team_id)
+        team_stats_df = team_stats.get_data_frames()[0]
+
+        # Filter the relevant columns for three-point attempts made (FG3A), made (FG3M), and percentage (FG3_PCT)
+        team_three_point_stats_df = pd.DataFrame({
+            'Year': team_stats_df['YEAR'],
+            'Three-Point Attempts': team_stats_df['FG3A'],
+            'Three-Point Made': team_stats_df['FG3M'],
+            'Three-Point Percentage': team_stats_df['FG3_PCT'],
+            'Playoff Wins': team_stats_df['PO_WINS']
         })
 
-    conn.close()
-    return teams_data
+        # Convert the 'Year' column to integers representing the starting year of the season
+        team_three_point_stats_df['Year'] = team_three_point_stats_df['Year'].str[:4].astype(int)
+
+        return team_three_point_stats_df.to_dict(orient='records')
+    except:
+        return []
+
+# @app.route('/team/<int:team_id>', methods=['GET'])
+@app.route('/team/<team_id>', methods=['GET'])
+def get_team_stats(team_id):
+    team_data = get_team_data(team_id)
+    if not team_data:
+        return f"Team data for team ID {team_id} not found.", 404
+    return jsonify({
+        'Team Name': team_names.get(team_id, 'Unknown'),
+        'Team Data': team_data
+    })
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
-@app.route('/data')
-def get_data():
-    teams_data = fetch_team_stats()
-    return jsonify(teams_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
